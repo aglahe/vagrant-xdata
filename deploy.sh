8d75f2b822496f8a8b1613376ced9ef72fbb191c
Aaron Glahe <aaron.glahe@gmail.com>
2015-03-12 15:06:40 -0400

    Added ipython notebook

--- deploy.sh
+++ deploy.sh
@@ -62,11 +62,11 @@ sudo ln -s /var/log/storm /srv/software/storm/logs
 sudo ln -s /etc/storm/conf /srv/software/storm/conf
 sudo mkdir -p /tmp/storm/local
 
-# Get Scala 2.10.4
-sudo wget http://www.scala-lang.org/files/archive/scala-2.10.4.tgz
-sudo /bin/tar xzvf scala-2.10.4.tgz -C /usr/lib
-sudo ln -s /usr/lib/scala-2.10.4 /usr/lib/scala
-sudo echo "export PATH=\$PATH:/usr/lib/scala/bin" >> /etc/profile.d/scala.sh
+# Get Scala 2.11.5
+sudo wget http://www.scala-lang.org/files/archive/scala-2.11.5.tgz
+sudo /bin/tar xzvf scala-2.11.5.tgz -C /usr/lib
+sudo ln -s /usr/lib/scala-2.11.5 /usr/lib/scala
+sudo echo "export PATH=\$PATH:/usr/lib/scala/bin\nexport SCALA_HOME=/usr/lib/scala" >> /etc/profile.d/scala.sh
 
 # Get MiniConda
 sudo wget http://repo.continuum.io/miniconda/Miniconda-3.7.0-Linux-x86_64.sh
@@ -75,3 +75,12 @@ sudo ./Miniconda-3.7.0-Linux-x86_64.sh -b -p /srv/software/anaconda
 sudo echo "export PATH=/srv/software/anaconda/bin:\$PATH" >> /etc/profile.d/anaconda.sh
 sudo /srv/software/anaconda/bin/conda update --yes conda
 sudo /srv/software/anaconda/bin/conda install --yes ipython
+sudo /srv/software/anaconda/bin/conda install --yes ipython-notebook
+
+# create hadoop config files
+HDFS_COMPUTE_CONF=/etc/hadoop/conf.memex
+alternatives --install /etc/hadoop/conf hadoop-conf $HDFS_COMPUTE_CONF 60
+
+# Add the Hadoop Lzo
+sudo wget -P /etc/yum.repos.d http://archive-primary.cloudera.com/gplextras5/redhat/6/x86_64/gplextras/cloudera-gplextras5.repo
+sudo yum -y install hadoop-lzo
